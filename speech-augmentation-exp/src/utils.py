7f40601c1ed435def849ca013d9b6a3da6f6a96e
Kankavee <kankaveeramsri22@gmail.com>
2026-04-07 09:31:49 +0700

    แปลง param ให้เหมาะกับการเอาใส่ชื่อไฟล์

--- speech-augmentation-exp/src/utils.py
+++ speech-augmentation-exp/src/utils.py
@@ -13,6 +13,23 @@ def save_audio(audio, sr, output_path):
     sf.write(output_path, audio, sr)
 
 
+def format_param_for_filename(param):
+    """
+    แปลง param ให้เหมาะกับการเอาไปใส่ชื่อไฟล์
+    """
+    if isinstance(param, dict):
+        pause_prob = str(param["pause_prob"]).replace(".", "_")
+        pause_range = param["pause_ms_range"]
+        return f"p{pause_prob}_d{pause_range[0]}-{pause_range[1]}"
+
+    elif isinstance(param, float):
+        return str(param).replace(".", "_")
+
+    else:
+        return str(param)
+
+
 def make_output_filename(original_name, aug_type, param):
     base = os.path.splitext(original_name)[0]
-    return f"{base}__{aug_type}_{param}.wav"
+    param_str = format_param_for_filename(param)
+    return f"{base}__{aug_type}_{param_str}.wav"
